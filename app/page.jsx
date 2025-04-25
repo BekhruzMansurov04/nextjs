b26618e8db20aa0e649cc418015419b9766461d2
BekhruzMansurov <mansurovbehruz41@gmail.com>
2025-04-25 15:08:59 +0500

    "nex"

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import Dashboard from './dashboard';
+ 
  export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
        <h2>Hello Next JS</h2>
+       <Dashboard />
     </div>
   );
 }
